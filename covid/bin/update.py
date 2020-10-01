20e88481cc9c6ce46cd9d51793797601a39ff25a
Steven K <steven@pointcircle.com>
2020-10-01 14:39:47 -0700

    Add WI to this mix

--- covid/bin/update.py
+++ covid/bin/update.py
@@ -46,5 +46,28 @@ def main():
             writer.writerow(row)
 
 
+    start_date = datetime.date(2020,3, 10)
+    data_type = 'cases'
+
+    areas = [
+        {'state': 'wisconsin'},
+        # {'county': 'new york city'},
+        # {'county': 'los angeles'},
+        # {'county': 'snohomish'}
+    ]
+
+    results = []
+    for area in areas:
+        results += get_combined_row_data(start_date=start_date, data_types=data_type,
+                                         my_county="rock",  **area,)
+
+    output = os.path.expanduser('~/Downloads/wi-covid_info-%s.csv' % datetime.date.today())
+    with open(output, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=results[0].keys())
+        writer.writeheader()
+        for row in results:
+            writer.writerow(row)
+
+
 if __name__ == '__main__':
     sys.exit(main())
